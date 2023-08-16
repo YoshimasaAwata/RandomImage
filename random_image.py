8bf7ee3c753dcd9d86b028747a889d4d8745b1ca
Yoshiamasa Awata <yoshia.awata@nifty.ne.jp>
2023-08-16 17:52:37 +0900

    BOX状のノイズ

--- random_image.py
+++ random_image.py
@@ -1,9 +1,18 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from PIL import Image
 
 plt.ion()
 
-rimage = np.random.randint(0, 256, (512, 512, 3))
-plt.imshow(rimage)
+width = 768
+height = 512
+block_size = 4
+rimage = np.random.randint(
+    0, 256, (int(height / block_size), int(width / block_size), 3)
+)
+rimage = rimage.astype(np.uint8)
+src_img = Image.fromarray(rimage)
+dst_img = src_img.resize((width, height), resample=Image.BILINEAR)
+plt.imshow(dst_img)  # type: ignore
 
 print("done")
